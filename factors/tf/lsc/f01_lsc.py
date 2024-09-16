2a6a92c85341d8b29f53dc03c7e3e2080cf1edd8
tianfang <fieldput1994@163.com>
2024-09-16 17:22:36 +0800

    lsc

---
+++ factors/tf/lsc/f01_lsc.py
@@ -0,0 +1,345 @@
+
+"""
+
+Author: TIAN Fang
+Date: Sep 12, 2024
+
+"""
+
+# %% imports
+import os
+import sys
+import traceback
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+from copy import deepcopy
+from time import time, sleep
+from datetime import timedelta
+from collections import defaultdict
+
+# % % add sys path
+file_path = Path(__file__).resolve()
+file_dir = file_path.parents[0]
+project_dir = file_path.parents[1]
+sys.path.append(str(project_dir))
+
+# % %
+from core.factor_updater import FactorUpdater
+from core.task_scheduler import TaskScheduler
+from core.cache_persist_manager import CacheManager, PersistenceManager
+from core.immediate_process_manager import ImmediateProcessManager
+from utils.datautils import add_row_to_dataframe_reindex
+from utils.data_parser import convert_to_lowercase
+from utils.decorator_utils import run_by_thread
+from utils.timeutils import parse_time_string
+
+
+# % % cache & persist
+class MyCacheMgr(CacheManager):
+    
+    def __init__(self, params, param_set, cache_dir, cache_lookback, log=None):
+        super().__init__(params, param_set, cache_dir, cache_lookback, log=log)
+        self.cache_mapping = defaultdict(list)
+        self._init_containers()
+    
+    def _init_containers(self):
+        self.cache = defaultdict(list)
+    
+    def _init_cache_mapping(self):
+        amount_type_list = self.params['factors_related']['amount_type']
+        for amount_type in amount_type_list:
+            cache_path = self.cache_dir / f'cache_1min_{amount_type}.parquet'
+            self.cache_mapping[amount_type]['path'] = cache_path
+            self.cache_mapping[amount_type]['container'] = self.cache
+            self.cache_mapping[amount_type]['key'] = amount_type # 和mapping的键值一样，但若有多个容器，就可能不同
+            
+            
+class MyPersistenceMgr(PersistenceManager):
+    
+    def _init_persist_mapping(self):
+        for pr in self.param_set:
+            pr_name = pr['name']
+            pr_persist_dir = self.persist_dir / pr_name
+            pr_persist_dir.mkdir(parents=True, exist_ok=True)
+            self.persist_mapping[pr_name]['dir'] = pr_persist_dir
+            self.persist_mapping[pr_name]['container'] = self.factor_persist
+            self.persist_mapping[pr_name]['key'] = pr_name
+        ts_persist_dir = self.persist_dir / 'update_time'
+        ts_persist_dir.mkdir(parents=True, exist_ok=True)
+        self.persist_mapping['update_time']['dir'] = ts_persist_dir
+        self.persist_mapping['update_time']['container'] = self.factor_persist
+        self.persist_mapping['update_time']['key'] = 'update_time'
+        
+        
+# %% immediate process
+class MyImmediateProcessMgr(ImmediateProcessManager):
+    
+    # 此处未定义__init__，实际使用时，若即时计算需要用到某些参数，可以设置__init__传入
+    
+    def _init_container(self):
+        # self.factor = {'volume': defaultdict(list), 'amount': defaultdict(list)}
+        self.factor = defaultdict(list)
+        self.trade = defaultdict(list) # 存储每个symbol对应的逐笔trade数据
+        self.update_time = {} # symbol最后的更新时间
+    
+    def _init_topic_func_mapping(self):
+        # self.topic_func_mapping['CCBar'] = self._process_cc_bar
+        self.topic_func_mapping['CCTrade'] = self._process_cc_trade
+        
+    def _process_cc_bar(self, pb_msg):
+        header = pb_msg.header
+        symbol = convert_to_lowercase(header.symbol)
+        ts = header.timestamp
+        
+        print(pb_msg.header.timestamp, pb_msg.header.symbol, pb_msg.bar.open, pb_msg.bar.close)
+        
+        # self.factor['total'][symbol] = imb
+        self.update_time[symbol] = ts
+    
+    def _process_cc_trade(self, pb_msg):
+        header = pb_msg.header
+        symbol = convert_to_lowercase(header.symbol)
+        ts = header.timestamp
+        
+        # print(pb_msg.header, pb_msg.trade)
+        
+        # self.factor['volume'][symbol].append(pb_msg.trade)
+        self.factor['trade'].append((symbol, ts, pb_msg.trade))
+        # self.factor[symbol].append(pb_msg.trade)
+        # self.trade[symbol].append(pb_msg.trade)
+        self.update_time[symbol] = ts
+
+# %%
+class F01Lsc(FactorUpdater):
+    
+    name = 'f01_lsc'
+    
+    def __init__(self):
+        super().__init__()
+        
+        self._init_param_names() # useless?
+        self._init_lookback_mapping()
+        self._init_managers()
+        
+        self._init_task_scheduler()
+        self._add_tasks()
+        self.size_labels = list('SMLX') # must be monotonic
+        self.volume_types = ['amount', 'volume', 'tradenum']
+        self.sides = ['B', 'S']
+        self.side_size_labels = [f'{side}_Quantile_{st}' for side in self.sides for st in self.size_labels]
+        self.cahe_varnames = [f'{vt}_{sst}' for vt in self.volume_types for sst in self.side_size_labels]
+        self.cache_vars = defaultdict(pd.DataFrame)
+        self._load_init_cahe()
+        self._load_size_threshold()
+        self.min_lookback = 1440 # 保存分钟数据的最大保存长度
+        
+        self.persist_vars = defaultdict(pd.DataFrame)
+        self.persist_varnames = [f'{vt}_{fn}' for vt in self.volume_types for fn in ['lscb', 'lscs', 'lsc']]
+        self._load_init_persist()
+        
+        
+    def _init_param_names(self):
+        for pr in self.param_set:
+            amount_type = pr['amount_type']
+            mmt_wd = pr['mmt_wd']
+            pr['name'] = f'{amount_type}_mmt{mmt_wd}_ma' # 因子名
+        
+    def _init_lookback_mapping(self):
+        cache_period = self.params['record']['cache_period']
+        mmt_wd_list = self.params['factors_related']['mmt_wd']
+        
+        self.cache_lookback = timedelta(seconds=parse_time_string(cache_period))
+        self.mmt_wd_lookback_mapping = {mmt_wd: timedelta(seconds=parse_time_string(mmt_wd))
+                                        for mmt_wd in mmt_wd_list}
+    
+    def _load_size_threshold(self):
+        """ load trade and order size thresholds """
+        trade_size_threshold = pd.read_parquet(os.path.join(self.path_config['size_params'], 'trade_size_threshold.parquet'))
+        self.trade_size_threshold = trade_size_threshold.T.to_dict('list')
+        del trade_size_threshold
+        
+    def _init_managers(self):
+        # 即时记录
+        self.immediate_mgr = MyImmediateProcessMgr(self.topic_list, self.msg_controller, log=None)
+        # 定时记录
+        self.cache_mgr = MyCacheMgr(self.params, self.param_set, self.cache_dir,
+                                    self.cache_lookback, log=self.log)
+        self.persist_mgr = MyPersistenceMgr(self.params, self.param_set, self.persist_dir, log=self.log)
+    
+    def _load_init_cahe(self):
+        for varname in self.cahe_varnames:
+            try:
+                self.cache_vars[varname] = pd.read_parquet(self.cache_dir / f'{varname}.parquet')
+            except Exception:
+                self.cache_vars[varname] = pd.DataFrame()
+    
+    def _load_init_persist(self):
+        for fn in self.persist_varnames:
+            try:
+                self.persist_vars[fn] = pd.read_parquet(self.persist_dir / f'{fn}.parquet')
+            except Exception:
+                self.persist_vars[fn] = pd.DataFrame()
+    
+    # @run_by_thread
+    def _save_cache(self, max_rowno: int):
+        dc_cache_vars = deepcopy(self.cache_vars)
+        for varname, df in dc_cache_vars.items():
+            df.iloc[-max_rowno:].to_parquet(self.cache_dir / f'{varname}.parquet')
+    
+    # @run_by_thread
+    def _save_persist(self):
+        dc_persist_vars = deepcopy(self.persist_vars)
+        for varname, df in dc_persist_vars.items():
+            df.to_parquet(self.persist_dir / f'{varname}.parquet')
+    
+    def _init_task_scheduler(self):
+        self.task_scheduler = {name: TaskScheduler(log=self.log) for name in ['calc', 'io']}
+    
+    def _add_tasks(self):
+        # 按同时触发时预期的执行顺序排列
+        # 本部分需要集成各类参数与mgr，故暂不做抽象
+        # 此处时间参数应为1min和30min，为了测试更快看到结果，暂改为1min -> 3s，30min -> 1min
+        self.task_scheduler['calc'].add_task("1 Minute Record", 'second', 60, self._minute_record)
+        # self.task_scheduler.add_task("30 Minutes Final and Send", 'minute', 1, self._half_hour_record_n_send)
+        self.task_scheduler['io'].add_task("5 Minute Save to Cache", 'minute', 5, self._5min_save_to_cache)
+        self.task_scheduler['io'].add_task("30 Minutes Calculate Factor & Save to Persist", 'minute', 5, self._half_hour_save_factor)
+        # self.task_scheduler.add_task("30 Minutes Save to Persist", 'minute', 5, self._half_hour_save_to_final)
+        
+    def _minute_record(self, ts):
+        for amount_type in sorted(self.immediate_mgr.factor.keys()):
+            
+            print(amount_type, len(self.cache_mgr.cache_mapping[amount_type]),
+                  len(self.immediate_mgr.factor[amount_type]))
+            factor_amount_type = self.immediate_mgr.factor[amount_type]
+            fetch_length = len(factor_amount_type) # 此时切出来的数据大小
+            self.cache_mgr.cache_mapping[amount_type] = factor_amount_type
+            
+            # 清空fetch过的数据
+            print(amount_type, fetch_length, len(self.cache_mgr.cache_mapping[amount_type]),
+                  len(self.immediate_mgr.factor[amount_type]))
+            self.immediate_mgr.factor[amount_type] = self.immediate_mgr.factor[amount_type][fetch_length:]
+            print(amount_type, fetch_length, len(self.cache_mgr.cache_mapping[amount_type]),
+                  len(self.immediate_mgr.factor[amount_type]))
+            
+            # 逐symbol计算大小单
+            cache_trade = defaultdict(list)
+            for symbol, _, batch_trade in self.cache_mgr.cache_mapping[amount_type]:
+                if len(batch_trade) > 0:
+                    for trade in batch_trade:
+                        cache_trade[symbol].append((trade.timestamp, trade.side, trade.volume, trade.amount))
+            
+            amount, volume, tradenum = defaultdict(list), defaultdict(list), defaultdict(list)
+            for symbol in sorted(cache_trade.keys()):
+                total_sides = defaultdict(str)
+                total_amount = defaultdict(float)
+                total_volume = defaultdict(float)
+                for trade in cache_trade[symbol]:
+                    total_sides[(trade[0], trade[1])] = trade[1]
+                    total_volume[(trade[0], trade[1])] += trade[2]
+                    total_amount[(trade[0], trade[1])] += trade[3]
+                total_sides = np.array(list(total_sides.values()))
+                total_volume = np.array(list(total_volume.values()))
+                total_amount = np.array(list(total_amount.values()))
+                
+                # use total_amonut to distinguish large and small trades
+                this_trade_size_thresh = self.trade_size_threshold.get(symbol, [100, 10000, 100000])
+                categories = np.digitize(total_amount, bins=[0] + this_trade_size_thresh + [np.inf])
+                
+                amt, vlm, tdn = np.zeros(8), np.zeros(8), np.zeros(8)
+                for sk in range(2):
+                    is_this_side = total_sides == self.sides[sk]
+                    for ck in range(4):
+                        is_this_cat = (categories == ck + 1) & is_this_side
+                        amt[ck + sk * 4] += np.sum(total_amount[is_this_cat])
+                        vlm[ck + sk * 4] += np.sum(total_volume[is_this_cat])
+                        tdn[ck + sk * 4] = np.sum(is_this_cat)
+                amount[symbol], volume[symbol], tradenum[symbol] = amt, vlm, tdn
+            amount = pd.DataFrame.from_dict(amount, orient='index', columns=self.side_size_labels)
+            volume = pd.DataFrame.from_dict(volume, orient='index', columns=self.side_size_labels)
+            tradenum = pd.DataFrame.from_dict(tradenum, orient='index', columns=self.side_size_labels).astype(int)
+            
+            # save trade_size one by one
+            for vt, vl in zip(['amount', 'volume', 'tradenum'], [amount, volume, tradenum]):
+                for sst in self.side_size_labels:
+                    self.cache_vars[f'{vt}_{sst}'] = add_row_to_dataframe_reindex(df=self.cache_vars[f'{vt}_{sst}'],
+                                                                                  new_data=vl[sst], index=ts)
+            
+    def _half_hour_record_n_send(self, ts):
+        for pr in self.param_set:
+            amount_type = pr['amount_type']
+            mmt_wd = pr['mmt_wd']
+            pr_name = pr['name']
+            factor_per_minute = self.cache_mgr.cache[amount_type]
+            mmt_wd_lookback = self.mmt_wd_lookback_mapping[mmt_wd]
+            factor_ma = factor_per_minute.loc[ts-mmt_wd_lookback:].mean(axis=0)
+            self.db_handler.batch_insert_data(self.author, self.category, pr_name, factor_ma)
+            self.persist_mgr.add_row(pr_name, factor_ma, ts)
+            
+        self.persist_mgr.add_row('update_time', self.immediate_mgr.update_time, ts)
+
+    def _5min_save_to_cache(self, ts):
+        sleep(20) # wait 1min data re-organization
+        self._save_cache(max_rowno=self.min_lookback) # save the latest max_rowno rows
+        print(f'{ts}: cache saved')
+    
+    def _half_hour_save_factor(self, ts):
+        sleep(30)
+        temp_vars = defaultdict(pd.DataFrame)
+        dc_cache_vars = deepcopy(self.cache_vars)
+        for vt in self.volume_types:
+            temp_vars[f'{vt}_bs'] = dc_cache_vars[f'{vt}_B_Quantile_S'].iloc[-self.min_lookback:]
+            temp_vars[f'{vt}_ss'] = dc_cache_vars[f'{vt}_S_Quantile_S'].iloc[-self.min_lookback:]
+            temp_vars[f'{vt}_bl'] = dc_cache_vars[f'{vt}_B_Quantile_L'].iloc[-self.min_lookback:]
+            temp_vars[f'{vt}_sl'] = dc_cache_vars[f'{vt}_S_Quantile_L'].iloc[-self.min_lookback:]
+        del dc_cache_vars
+        
+        for vt in self.volume_types:
+            com_idx = sorted(set(temp_vars[f'{vt}_bs'].index.tolist() + temp_vars[f'{vt}_ss'].index.tolist() +
+                                 temp_vars[f'{vt}_bl'].index.tolist() + temp_vars[f'{vt}_sl'].index.tolist()))
+            com_col = sorted(set(temp_vars[f'{vt}_bs'].columns.tolist() + temp_vars[f'{vt}_ss'].columns.tolist() +
+                                 temp_vars[f'{vt}_bl'].columns.tolist() + temp_vars[f'{vt}_sl'].columns.tolist()))
+            
+            temp_vars[f'{vt}_bs'] = temp_vars[f'{vt}_bs'].reindex(index=com_idx, columns=com_col).fillna(0)
+            temp_vars[f'{vt}_ss'] = temp_vars[f'{vt}_ss'].reindex(index=com_idx, columns=com_col).fillna(0)
+            temp_vars[f'{vt}_bl'] = temp_vars[f'{vt}_bl'].reindex(index=com_idx, columns=com_col).fillna(0)
+            temp_vars[f'{vt}_sl'] = temp_vars[f'{vt}_sl'].reindex(index=com_idx, columns=com_col).fillna(0)
+            
+            vt_s = temp_vars[f'{vt}_bs'] + temp_vars[f'{vt}_ss']
+            vt_l = temp_vars[f'{vt}_bl'] + temp_vars[f'{vt}_sl']
+            
+            # active_buy, active_sell, both corr
+            lscb = temp_vars[f'{vt}_bs'].corrwith(temp_vars[f'{vt}_bl'], axis=0, method='spearman')
+            lscs = temp_vars[f'{vt}_ss'].corrwith(temp_vars[f'{vt}_sl'], axis=0, method='spearman')
+            lsc = vt_s.corrwith(vt_l, axis=0, method='spearman')
+            
+            self.persist_vars[f'{vt}_lscb'] = add_row_to_dataframe_reindex(df=self.persist_vars[f'{vt}_lscb'], new_data=lscb, index=ts)
+            self.persist_vars[f'{vt}_lscs'] = add_row_to_dataframe_reindex(df=self.persist_vars[f'{vt}_lscs'], new_data=lscs, index=ts)
+            self.persist_vars[f'{vt}_lsc'] = add_row_to_dataframe_reindex(df=self.persist_vars[f'{vt}_lsc'], new_data=lsc, index=ts)
+        
+        self._save_persist() # save the calculated factors
+        
+        
+    def _half_hour_save_to_final(self, ts):
+        self.persist_mgr.save(ts)
+
+    def run(self):
+        self.msg_controller.start() # 占一条线程，用于收取lord消息存队列
+        self.immediate_mgr.start() # 占一条线程，用于即时处理队列消息
+        self.task_scheduler['io'].start() # 主线程：处理任务 + 辅助线程：任务调度
+        self.task_scheduler['calc'].start(use_thread_for_task_runner=False) # 主线程：处理任务 + 辅助线程：任务调度
+        
+    def stop(self):
+        self._running = False
+        self.msg_controller.stop()
+        self.immediate_mgr.stop()
+        for task_name, task_scheduler in self.task_scheduler.items():
+            task_scheduler.stop()
+    
+        
+# %%
+if __name__=='__main__':
+    test = F01Lsc()    
+    test.run()
+    
