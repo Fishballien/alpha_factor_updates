289fa2b2fb8f15d00be834c85e408481b1f1cf4f
fishballien <fishballien@outlook.com>
2024-09-20 15:27:29 +0800

    feat: modify data with lock

--- core/cache_persist_manager.py
+++ core/cache_persist_manager.py
@@ -18,6 +18,7 @@ from collections import defaultdict
 from abc import ABC, abstractmethod
 import h5py
 import numpy as np
+import threading
 
 
 from utils.timeutils import get_curr_utc_date, get_date_based_on_timestamp
@@ -31,6 +32,11 @@ class DataManager(ABC):
         self.params = params
         self.param_set = param_set
         self.log = log
+        
+        self._init_locks()
+        
+    def _init_locks(self):
+        self.locks = defaultdict(threading.Lock)
 
     def _load_or_init_batch(self, path, container, keys, type_name='Path'):
         if not path.exists():
@@ -119,7 +125,8 @@ class CacheManager(DataManager):
         if self.file_name is None:
             return  # 如果没有自定义文件名，跳过保存
         for save_name in self.cache_mapping.values():  # 使用保存名进行操作
-            self._cut_cache(save_name, ts)  # 保留 cut_cache 步骤
+            with self.locks[save_name]:
+                self._cut_cache(save_name, ts)  # 保留 cut_cache 步骤
         
         # 批量保存到 HDF5 文件，直接传 self.cache 并使用保存名
         path = self.cache_dir / f'{self.file_name}.h5'
@@ -127,7 +134,8 @@ class CacheManager(DataManager):
             
     def add_row(self, access_name, new_row, index):
         save_name = self.cache_mapping[access_name]
-        self._add_row(self.cache, save_name, new_row, index)
+        with self.locks[save_name]:
+            self._add_row(self.cache, save_name, new_row, index)
         
     def __getitem__(self, access_name):
         save_name = self.cache_mapping[access_name]
@@ -166,10 +174,22 @@ class PersistenceManager(DataManager):
     def save(self, ts):
         date_to_save = get_date_based_on_timestamp(ts)  # 根据传入时间戳生成日期
         for key in self.persist_list:
-            self.factor_persist[key] = self.factor_persist[key].loc[date_to_save:]  # 保存当天数据
+            with self.locks[key]:
+                self.factor_persist[key] = self.factor_persist[key].loc[date_to_save:]  # 保存当天数据
         
         path = self.persist_dir / f'{date_to_save}.h5'
         self._save_to_h5_batch(path, self.factor_persist)
             
     def add_row(self, key, new_row, index):
-        self._add_row(self.factor_persist, key, new_row, index)
+        with self.locks[key]:
+            self._add_row(self.factor_persist, key, new_row, index)
+        
+        
+# %%
+class GeneralPersistenceMgr(PersistenceManager):
+    
+    def init_persist_list(self):
+        for pr in self.param_set:
+            pr_name = pr['name']
+            self.persist_list.append(pr_name)
+        self.persist_list.append('update_time')
